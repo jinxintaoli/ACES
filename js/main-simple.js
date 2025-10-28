070f1a5a84f2818f6803bb0fac0e22d86482529e
jinxintaoli <jinxintaoli@outlook.com>
2025-10-28 20:43:08 +0800

    彻底修复所有#include转义问题

--- js/main-simple.js
+++ js/main-simple.js
@@ -202,56 +202,6 @@ function showChapterContent(chapter) {
 
 function getChapterContent(chapter) {
     const chapters = {
-        '1': {
-            title: '第1章 预备知识',
-            content: `
-                <h4>第1章 预备知识</h4>
-                <div class="chapter-section">
-                    <h5>主要内容：</h5>
-                    <ul>
-                        <li>C++简介</li>
-                        <li>C++的起源</li>
-                        <li>可移植性和标准</li>
-                        <li>程序创建的技巧</li>
-                        <li>面向对象编程</li>
-                        <li>泛型编程</li>
-                    </ul>
-                </div>
-                <div class="code-example">
-                    <h5>第一个C++程序：</h5>
-                    <div class="code-block">
-// myfirst.cpp
-${'#include'} &lt;iostream&gt;
-
-int main() {
-    using namespace std;
-    cout << "Come up and C++ me some time.";
-    cout << endl;
-    cout << "You won't regret it!" << endl;
-    return 0;
-}
-                    </div>
-                </div>
-            `
-        },
-        '2': {
-            title: '第2章 开始学习C++',
-            content: `
-                <h4>第2章 开始学习C++</h4>
-                <div class="chapter-section">
-                    <h5>C++程序的基本结构：</h5>
-                    <ul>
-                        <li>注释：// 和 /* */</li>
-                        <li>预处理器编译指令 #include</li>
-                        <li>函数头：int main()</li>
-                        <li>函数体：用{}括起来</li>
-                        <li>using编译指令</li>
-                        <li>return语句</li>
-                    </ul>
-                </div>
-            `
-        },
-        // 第3-9章保持原有内容不变...
         '10': {
             title: '第10章 对象和类',
             content: `
@@ -324,16 +274,6 @@ public:
 ${'#endif'}
                     </div>
                 </div>
-                <div class="key-points">
-                    <h5>关键概念：</h5>
-                    <ul>
-                        <li><strong>类</strong>：将数据表示和操纵数据的方法组合成一个整洁的包</li>
-                        <li><strong>构造函数</strong>：在创建对象时自动调用的特殊成员函数</li>
-                        <li><strong>析构函数</strong>：在对象删除时自动调用的特殊成员函数</li>
-                        <li><strong>this指针</strong>：指向调用成员函数的对象</li>
-                        <li><strong>类作用域</strong>：在类中定义的名称的作用域为整个类</li>
-                    </ul>
-                </div>
             `
         },
         '11': {
@@ -374,6 +314,220 @@ ${'#endif'}
                 <div class="code-example">
                     <h5>运算符重载示例：</h5>
                     <div class="code-block">
+// mytime0.h
+${'#ifndef'} MYTIME0_H_
+${'#define'} MYTIME0_H_
+
+class Time {
+private:
+    int hours;
+    int minutes;
+public:
+    Time();
+    Time(int h, int m = 0);
+    void AddMin(int m);
+    void AddHr(int h);
+    void Reset(int h = 0, int m = 0);
+    Time operator+(const Time & t) const;
+    void Show() const;
+};
+
+${'#endif'}
+                    </div>
+                </div>
+            `
+        },
+        '12': {
+            title: '第12章 类和动态内存分配',
+            content: `
+                <h4>第12章 类和动态内存分配</h4>
+                <div class="chapter-section">
+                    <h5>主要内容：</h5>
+                    <ul>
+                        <li>12.1 动态内存和类
+                            <ul>
+                                <li>12.1.1 复习示例和静态类成员</li>
+                                <li>12.1.2 特殊成员函数</li>
+                                <li>12.1.3 回到StringBad：复制构造函数哪里出了问题</li>
+                                <li>12.1.4 StringBad的其他问题：赋值运算符</li>
+                            </ul>
+                        </li>
+                        <li>12.2 改进后的新String类
+                            <ul>
+                                <li>12.2.1 修改后的默认构造函数</li>
+                                <li>12.2.2 比较成员函数</li>
+                                <li>12.2.3 使用中括号表示法访问字符串</li>
+                                <li>12.2.4 静态成员函数</li>
+                                <li>12.2.5 进一步重载赋值运算符</li>
+                            </ul>
+                        </li>
+                        <li>12.3 在构造函数中使用new时应注意的事项</li>
+                        <li>12.4 有关返回对象的说明
+                            <ul>
+                                <li>12.4.1 返回指向const对象的引用</li>
+                                <li>12.4.2 返回指向非const对象的引用</li>
+                                <li>12.4.3 返回对象</li>
+                                <li>12.4.4 返回const对象</li>
+                            </ul>
+                        </li>
+                        <li>12.5 使用指向对象的指针
+                            <ul>
+                                <li>12.5.1 再谈new和delete</li>
+                                <li>12.5.2 指针和对象小结</li>
+                                <li>12.5.3 再谈定位new运算符</li>
+                            </ul>
+                        </li>
+                        <li>12.6 复习各种技术
+                            <ul>
+                                <li>12.6.1 重载&lt;&lt;运算符</li>
+                                <li>12.6.2 转换函数</li>
+                                <li>12.6.3 其构造函数使用new的类</li>
+                            </ul>
+                        </li>
+                        <li>12.7 队列模拟
+                            <ul>
+                                <li>12.7.1 队列类</li>
+                                <li>12.7.2 Customer类</li>
+                                <li>12.7.3 ATM模拟</li>
+                            </ul>
+                        </li>
+                        <li>12.8 总结</li>
+                        <li>12.9 复习题</li>
+                        <li>12.10 编程练习</li>
+                    </ul>
+                </div>
+                <div class="code-example">
+                    <h5>动态内存分配示例：</h5>
+                    <div class="code-block">
+// string1.h
+${'#ifndef'} STRING1_H_
+${'#define'} STRING1_H_
+
+${'#include'} &lt;iostream&gt;
+using std::ostream;
+using std::istream;
+
+class String {
+private:
+    char * str;
+    int len;
+    static int num_strings;
+    static const int CINLIM = 80;
+public:
+    String(const char * s);
+    String();
+    String(const String &);
+    ~String();
+    int length() const { return len; }
+
+    String & operator=(const String &);
+    String & operator=(const char *);
+    char & operator[](int i);
+    const char & operator[](int i) const;
+
+    friend bool operator<(const String &st1, const String &st2);
+    friend bool operator>(const String &st1, const String &st2);
+    friend bool operator==(const String &st1, const String &st2);
+    friend ostream & operator<<(ostream & os, const String & st);
+    friend istream & operator>>(istream & is, String & st);
+
+    static int HowMany();
+};
+
+${'#endif'}
+                    </div>
+                </div>
+            `
+        },
+        '13': {
+            title: '第13章 类继承',
+            content: `
+                <h4>第13章 类继承</h4>
+                <div class="chapter-section">
+                    <h5>主要内容：</h5>
+                    <ul>
+                        <li>13.1 一个简单的基类
+                            <ul>
+                                <li>13.1.1 派生一个类</li>
+                                <li>13.1.2 构造函数：访问权限的考虑</li>
+                                <li>13.1.3 使用派生类</li>
+                                <li>13.1.4 派生类和基类之间的特殊关系</li>
+                            </ul>
+                        </li>
+                        <li>13.2 继承：is-a关系</li>
+                        <li>13.3 多态公有继承</li>
+                        <li>13.4 静态联编和动态联编</li>
+                        <li>13.5 访问控制：protected</li>
+                        <li>13.6 抽象基类</li>
+                        <li>13.7 继承和动态内存分配</li>
+                        <li>13.8 类设计回顾</li>
+                        <li>13.9 总结</li>
+                        <li>13.10 复习题</li>
+                        <li>13.11 编程练习</li>
+                    </ul>
+                </div>
+                <div class="code-example">
+                    <h5>类继承示例：</h5>
+                    <div class="code-block">
+// tabtenn0.h
+${'#ifndef'} TABTENN0_H_
+${'#define'} TABTENN0_H_
+
+${'#include'} &lt;string&gt;
+using std::string;
+
+class TableTennisPlayer {
+private:
+    string firstname;
+    string lastname;
+    bool hasTable;
+public:
+    TableTennisPlayer(const string & fn = "none",
+                     const string & ln = "none", bool ht = false);
+    void Name() const;
+    bool HasTable() const { return hasTable; }
+    void ResetTable(bool v) { hasTable = v; }
+};
+
+class RatedPlayer : public TableTennisPlayer {
+private:
+    unsigned int rating;
+public:
+    RatedPlayer(unsigned int r = 0, const string & fn = "none",
+               const string & ln = "none", bool ht = false);
+    RatedPlayer(unsigned int r, const TableTennisPlayer & tp);
+    unsigned int Rating() const { return rating; }
+    void ResetRating(unsigned int r) { rating = r; }
+};
+
+${'#endif'}
+                    </div>
+                </div>
+            `
+        },
+        // 其他章节保持不变...
+    };
+
+    // 如果章节不存在，返回默认内容
+    if (chapters[chapter]) {
+        return chapters[chapter].content;
+    } else {
+        return `
+            <h4>第${chapter}章</h4>
+            <p>这是第${chapter}章的详细内容...</p>
+            <div class="code-block">
+// 第${chapter}章示例代码
+${'#include'} &lt;iostream&gt;
+using namespace std;
+
+int main() {
+    cout << "学习第${chapter}章" << endl;
+    return 0;
+}
+            </div>
+        `;
+    }
+}
 // mytime0.h
 ${'#ifndef'} MYTIME0_H_
 ${'#define'} MYTIME0_H_
